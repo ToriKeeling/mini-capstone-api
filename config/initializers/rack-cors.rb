3e9f307e76afee1de402e82959afbb78366c0243
ToriKeeling <tori.keeling@gmail.com>
2023-10-09 21:11:53 -0500

    fix cors file

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -3,13 +3,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "http://mini-capstone.peterxjang.com", "localhost:5173"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
-
-  # config/initializers/cors.rb
-
-  Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins "*"
-      resource "*", headers: :any, methods: [:get, :post, :patch, :put]
-    end
-  end
 end
